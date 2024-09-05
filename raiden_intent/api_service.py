818333a433f6abf6bf4fc7fc0a224f9cc86c6f55
MinhVu Local <rinputin482@gmail.com>
2024-09-05 14:31:27 +0700

    adjust: manipulate zalo request's cycle

--- raiden_intent/api_service.py
+++ raiden_intent/api_service.py
@@ -1,18 +1,18 @@
-# from classification import translator, loaded_model, loaded_token, intents_labels, predict_intents
-# from flask import Flask, request, jsonify
+from classification import translator, loaded_model, loaded_token, intents_labels, predict_intents
+from flask import Flask, request, jsonify
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
-# @app.route('/customer-intent', methods = ['POST'])
-# def intent_classification():
-#     data = request.get_json()
-#     customer_request = data["customer_message"] if data["customer_message"] else "No idea"
-#     customer_intent = predict_intents(customer_request, translator, loaded_model, loaded_token, intents_labels)
-#     return jsonify(
-#         {
-#             'customer_intent': customer_intent
-#         }
-#     )
+@app.route('/customer-intent', methods = ['POST'])
+def intent_classification():
+    data = request.get_json()
+    customer_request = data["customer_message"] if data["customer_message"] else "No idea"
+    customer_intent = predict_intents(customer_request, translator, loaded_model, loaded_token, intents_labels)
+    return jsonify(
+        {
+            'customer_intent': customer_intent
+        }
+    )
 
-# if __name__ == "__main__":
-#     app.run(debug= True)
+if __name__ == "__main__":
+    app.run(debug= True)
--- raiden_intent/classification.py
+++ raiden_intent/classification.py
@@ -1,54 +1,54 @@
-# import numpy as np
-# import pandas as pd
-# import torch
-# import torch.nn as nn
-# from torch.utils.data import DataLoader, TensorDataset
-# import torchvision.models as models
-# from transformers import AutoTokenizer, RobertaModel
-# from googletrans import Translator
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader, TensorDataset
+import torchvision.models as models
+from transformers import AutoTokenizer, RobertaModel
+from googletrans import Translator
 
-# intents_labels = {
-#     'technical': 0,
-#     'stat': 1,
-#     'contact': 2,
-# }
+intents_labels = {
+    'technical': 0,
+    'stat': 1,
+    'contact': 2,
+}
 
-# ro_model = RobertaModel.from_pretrained('roberta-base')
+ro_model = RobertaModel.from_pretrained('roberta-base')
 
-# class RoBertaIntentsClassifier(nn.Module):
-#     def __init__(self, bert_model, num_classes):
-#         super(RoBertaIntentsClassifier, self).__init__()
-#         self.bert = ro_model
-#         self.relu1 = nn.ReLU()
-#         self.relu2 = nn.ReLU()
-#         self.fc = nn.Linear(self.bert.config.hidden_size, num_classes)
+class RoBertaIntentsClassifier(nn.Module):
+    def __init__(self, bert_model, num_classes):
+        super(RoBertaIntentsClassifier, self).__init__()
+        self.bert = ro_model
+        self.relu1 = nn.ReLU()
+        self.relu2 = nn.ReLU()
+        self.fc = nn.Linear(self.bert.config.hidden_size, num_classes)
 
-#     def forward(self, input_ids, attention_mask):
-#         outputs = self.bert(input_ids, attention_mask=attention_mask)
-#         pooled_output = outputs.last_hidden_state[:, 0, :]
-#         x = self.relu1(pooled_output)
-#         x = self.relu2(x)
-#         logits = self.fc(x)
-#         return logits
+    def forward(self, input_ids, attention_mask):
+        outputs = self.bert(input_ids, attention_mask=attention_mask)
+        pooled_output = outputs.last_hidden_state[:, 0, :]
+        x = self.relu1(pooled_output)
+        x = self.relu2(x)
+        logits = self.fc(x)
+        return logits
     
-# loaded_token = AutoTokenizer.from_pretrained("intent_model", local_files_only=True)
-# loaded_model = RoBertaIntentsClassifier(ro_model, 3).to("cpu")
-# state_dict = torch.load(f"intent_model\\raiden_itents_classifier_model.pth", map_location=torch.device('cpu'))
-# loaded_model.load_state_dict(state_dict)
+loaded_token = AutoTokenizer.from_pretrained("intent_model", local_files_only=True)
+loaded_model = RoBertaIntentsClassifier(ro_model, 3).to("cpu")
+state_dict = torch.load(f"intent_model\\raiden_itents_classifier_model.pth", map_location=torch.device('cpu'))
+loaded_model.load_state_dict(state_dict)
 
-# translator = Translator()
+translator = Translator()
 
-# def predict_intents(request, translator, model, tokenizer, intents_labels):
-#     sentence = translator.translate(request, src='vi', dest='en').text
-#     encoded_sentence = tokenizer(sentence, padding=True, truncation=True, return_tensors="pt")
+def predict_intents(request, translator, model, tokenizer, intents_labels):
+    sentence = translator.translate(request, src='vi', dest='en').text
+    encoded_sentence = tokenizer(sentence, padding=True, truncation=True, return_tensors="pt")
 
-#     with torch.no_grad():
-#         logits = model(encoded_sentence['input_ids'], encoded_sentence['attention_mask'])
-#         predicted_label = torch.argmax(logits, dim=1).item()
+    with torch.no_grad():
+        logits = model(encoded_sentence['input_ids'], encoded_sentence['attention_mask'])
+        predicted_label = torch.argmax(logits, dim=1).item()
 
-#     predicted_intent = [k for k, v in intents_labels.items() if v == predicted_label][0]
+    predicted_intent = [k for k, v in intents_labels.items() if v == predicted_label][0]
 
-#     return predicted_intent
+    return predicted_intent
 
 # example
 # cus_request = "Hãy cho tôi thông tin thống kê về cuộc gọi tháng 3"
